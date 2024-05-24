4bf4b748c9ef34d35d1adaa9260d774017436161
AydenGrace <pa.ce.hepl@gmail.com>
2024-05-24 14:35:33 +0200

    Url

--- FRONT/src/components/VideoCard/VideoCard.jsx
+++ FRONT/src/components/VideoCard/VideoCard.jsx
@@ -194,6 +194,11 @@ export default function VideoCard({ Video }) {
           width="100%"
           height="100%"
           url={Video.url}
+          config={{
+            youtube: {
+              playerVars: { showinfo: 1 },
+            },
+          }}
           controls={true}
           className={`${styles.react_player}`}
         />

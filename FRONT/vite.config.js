0146209b91fc9f407fc436480b67ce39d549c19f
AydenGrace <pa.ce.hepl@gmail.com>
2024-05-24 14:34:09 +0200

    Url

--- FRONT/vite.config.js
+++ FRONT/vite.config.js
@@ -5,10 +5,10 @@ import react from "@vitejs/plugin-react-swc";
 export default defineConfig({
   plugins: [react()],
 
-  headers: {
-    "Strict-Transport-Security": "max-age=86400; includeSubDomains", // Adds HSTS options to your website, with a expiry time of 1 day
-    "X-Content-Type-Options": "nosniff", // Protects from improper scripts runnings
-    "X-Frame-Options": "DENY", // Stops your site being used as an iframe
-    "X-XSS-Protection": "1; mode=block", // Gives XSS protection to legacy browsers
-  },
+  // headers: {
+  //   "Strict-Transport-Security": "max-age=86400; includeSubDomains", // Adds HSTS options to your website, with a expiry time of 1 day
+  //   "X-Content-Type-Options": "nosniff", // Protects from improper scripts runnings
+  //   "X-Frame-Options": "DENY", // Stops your site being used as an iframe
+  //   "X-XSS-Protection": "1; mode=block", // Gives XSS protection to legacy browsers
+  // },
 });

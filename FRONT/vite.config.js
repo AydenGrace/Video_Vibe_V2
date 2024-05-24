5b4d144eef6a3114e378f6fd8a12cdf7b05f0fa8
AydenGrace <pa.ce.hepl@gmail.com>
2024-05-24 14:27:38 +0200

    Url

--- FRONT/vite.config.js
+++ FRONT/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     headers: {
       "Strict-Transport-Security": "max-age=86400; includeSubDomains", // Adds HSTS options to your website, with a expiry time of 1 day
       "X-Content-Type-Options": "nosniff", // Protects from improper scripts runnings
-      "X-Frame-Options": "ALLOW-FROM https://www.youtube.com", // Stops your site being used as an iframe
+      "X-Frame-Options": "DENY", // Stops your site being used as an iframe
       "X-XSS-Protection": "1; mode=block", // Gives XSS protection to legacy browsers
     },
   },
